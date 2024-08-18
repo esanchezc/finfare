8f0e872f7211d400dc97ebf16adf975ad5718ec5
Emanuel Sanchez <esanchezc.sqa@gmail.com>
2024-08-18 18:12:44 -0500

    Add test to retrieve stocks you may be interested in

--- pages/google_finance_page.py
+++ pages/google_finance_page.py
@@ -2,13 +2,20 @@ from seleniumpagefactory.Pagefactory import PageFactory
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class GoogleFinancePage(PageFactory):
-    def __init__(self, driver):
-        self.driver = driver
+    def __init__(self, driver: webdriver):
+        self.driver: driver = driver
 
-    locators = {"page_title": ("XPATH", "//a[@id='sdgBod']/span[text()='Finance']")}
+    locators = {
+        "page_title": ("XPATH", "//a[@id='sdgBod']/span[text()='Finance']"),
+        "watchlist_title": ("ID", "smart-watchlist-title"),
+        "watchlist_items": (By.CSS_SELECTOR, "#smart-watchlist-title + ul > li"),
+        "watchlist_symbol": (By.CSS_SELECTOR, "div.COaKTb")
+    }
 
     def is_title_visible(self):
         try:
@@ -17,3 +24,23 @@ class GoogleFinancePage(PageFactory):
             )
         except TimeoutException:
             return False
+
+    def get_watchlist_stock_symbols(self):
+        try:
+            WebDriverWait(self.driver, 10).until(EC.visibility_of(self.watchlist_title))
+
+            watchlist_items = self.driver.find_elements(*self.locators["watchlist_items"])
+
+            stock_symbols = []
+            for item in watchlist_items:
+                symbol_element = item.find_element(*self.locators["watchlist_symbol"])
+                stock_symbols.append(symbol_element.text.strip())
+
+            return stock_symbols
+
+        except TimeoutException:
+            print("Watchlist title not found or timed out")
+            return []
+        except Exception as e:
+            print(f"An error occurred while retrieving stock symbols: {str(e)}")
+            return []
--- tests/test_google_finance_page.py
+++ tests/test_google_finance_page.py
@@ -6,3 +6,7 @@ from pages.google_finance_page import GoogleFinancePage
 def test_open_google_finance_page(driver):
     finance_page = GoogleFinancePage(driver)
     finance_page.is_title_visible(), "Finance page title is not visible"
+
+def test_get_stock_symbols(driver):
+    finance_page = GoogleFinancePage(driver)
+    assert len(finance_page.get_watchlist_stock_symbols()) == 6, "Stock symbols not found"
