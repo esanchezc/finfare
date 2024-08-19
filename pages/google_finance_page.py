3a393a1ce573912dd1aa0fb6a963a3132b9aa6c3
Emanuel Sanchez <esanchezc.sqa@gmail.com>
2024-08-18 19:37:39 -0500

    Add typehints and make locators consistent

--- pages/google_finance_page.py
+++ pages/google_finance_page.py
@@ -1,5 +1,7 @@
+from typing import List
+
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,27 +9,30 @@ from seleniumpagefactory.Pagefactory import PageFactory
 
 
 class GoogleFinancePage(PageFactory):
-    def __init__(self, driver: webdriver):
-        self.driver: driver = driver
+    def __init__(self, driver: webdriver.Remote):
+        self.driver: webdriver.Remote = driver
 
     locators = {
-        "page_title": ("XPATH", "//a[@id='sdgBod']/span[text()='Finance']"),
-        "watchlist_title": ("ID", "smart-watchlist-title"),
+        "page_title": (By.XPATH, "//a[@id='sdgBod']/span[text()='Finance']"),
+        "watchlist_title": (By.ID, "smart-watchlist-title"),
         "watchlist_items": (By.CSS_SELECTOR, "#smart-watchlist-title + ul > li"),
         "watchlist_symbol": (By.CSS_SELECTOR, "div.COaKTb"),
     }
 
-    def is_title_visible(self):
+    def is_title_visible(self, timeout: int = 10) -> bool:
         try:
-            return WebDriverWait(self.driver, 10).until(
-                EC.visibility_of(self.page_title)
+            WebDriverWait(self.driver, timeout).until(
+                EC.visibility_of_element_located(self.locators["page_title"])
             )
+            return True
         except TimeoutException:
             return False
 
-    def get_watchlist_stock_symbols(self):
+    def get_watchlist_stock_symbols(self, timeout: int = 10) -> List[str]:
         try:
-            WebDriverWait(self.driver, 10).until(EC.visibility_of(self.watchlist_title))
+            WebDriverWait(self.driver, timeout).until(
+                EC.visibility_of_element_located(self.locators["watchlist_title"])
+            )
 
             watchlist_items = self.driver.find_elements(
                 *self.locators["watchlist_items"]
@@ -35,8 +40,13 @@ class GoogleFinancePage(PageFactory):
 
             stock_symbols = []
             for item in watchlist_items:
-                symbol_element = item.find_element(*self.locators["watchlist_symbol"])
-                stock_symbols.append(symbol_element.text.strip())
+                try:
+                    symbol_element = item.find_element(
+                        *self.locators["watchlist_symbol"]
+                    )
+                    stock_symbols.append(symbol_element.text.strip())
+                except NoSuchElementException:
+                    continue  # Skip this item if the symbol element is not found
 
             return stock_symbols
 
